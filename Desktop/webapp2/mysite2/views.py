3faeeccdb904e4392215d9f01f449d15274b249a
quiki <oshunboteo@gmail.com>
2019-02-26 22:29:23 +0100

    Basic build of the template portfolio

---
+++ Desktop/webapp2/mysite2/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request): 
+    return render(request, 'index.html')
+
+def about(request): 
+    return render(request, 'about.html')
+
+def portfolio(request): 
+    return render(request, 'portfolio.html')
+
+def contact(request):
+    return render(request, 'contact.html')
